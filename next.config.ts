769912b0c41b6f73e59fa528eee59c73c027ac78
menegattb <menegattb@gmail.com>
2025-10-01 14:22:24 -0300

    fix: configuração de domínios para imagens do YouTube
    - Adicionado remotePatterns no next.config.ts para img.youtube.com
    - Configurado suporte para diferentes formatos de thumbnail do YouTube
    - Incluído i.ytimg.com como domínio alternativo
    - Corrigido erro de Next.js Image com hostname não configurado
    - Agora as thumbnails do YouTube carregam corretamente

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/vi_webp/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
